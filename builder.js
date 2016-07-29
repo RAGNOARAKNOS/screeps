c6d07ca0b5219086287ad55b063f551a2dafce66
RAGNOARAKNOS <ragnocorp@googlemail.com>
2016-07-29 16:51:50 +0100

    closing braces...numbnuts

--- builder.js
+++ builder.js
@@ -7,33 +7,33 @@ var roleBuilder =
       creep.memory.building = false;
       creep.say('harvesting');
     }
-  }
 
-  if(!creep.memory.building && creep.carry.energy == creep.carryCapacity)
-  {
-    creep.memory.building = true;
-    creep.say('building');
-  }
-
-  if(creep.memory.building)
-  {
-    var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
-
-    if(targets.length)
+    if(!creep.memory.building && creep.carry.energy == creep.carryCapacity)
     {
-      if(creep.build(targets[0]) == ERR_NOT_IN_RANGE)
+      creep.memory.building = true;
+      creep.say('building');
+    }
+
+    if(creep.memory.building)
+    {
+      var targets = creep.room.find(FIND_CONSTRUCTION_SITES);
+
+      if(targets.length)
       {
-        creep.moveTo(targets[0]);
+        if(creep.build(targets[0]) == ERR_NOT_IN_RANGE)
+        {
+          creep.moveTo(targets[0]);
+        }
       }
     }
-  }
-  else
-  {
-    var sources = creep.room.find(FIND_SOURCES);
-    
-    if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
+    else
     {
-      creep.moveTo(sources[0]);
+      var sources = creep.room.find(FIND_SOURCES);
+      
+      if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
+      {
+        creep.moveTo(sources[0]);
+      }
     }
   }
 };
