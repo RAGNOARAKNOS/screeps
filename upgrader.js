433bdf10b71e97eb2c4114b66f5fe8d20d1d92bc
RAGNOARAKNOS <ragnocorp@googlemail.com>
2016-07-29 18:47:43 +0100

    upgraders chat now

--- upgrader.js
+++ upgrader.js
@@ -7,27 +7,25 @@ var roleUpgrader =
     {
       // switch state
       creep.memory.working = false;
-      console.log('P1');
+      creep.say('No energy left, time to collect some');
     }
     // if creep is harvesting energy but is full
     else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity)
     {
       // switch state
       creep.memory.working = true;
-      console.log('P2');
+      creep.say('So much energy, time to upgrade stuff!');
     }
 
     // if creep is supposed to transfer energy to the controller
     if (creep.memory.working == true)
     {
-      console.log('P3');
-
       // try to upgrade the controller
       if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE)
       {
         // if not in range, move towards the controller
         creep.moveTo(creep.room.controller);
-        console.log('P4');
+        creep.say('To the controller!');
       }
     }
     // if creep is supposed to harvest energy from source
@@ -38,7 +36,7 @@ var roleUpgrader =
       // try to harvest energy, if the source is not in range
       if (creep.harvest(source) == ERR_NOT_IN_RANGE)
       {
-        console.log('P5');
+        creep.say('To the energy!');
         // move towards the source
         creep.moveTo(source);
       }
