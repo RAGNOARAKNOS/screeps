292f7d09edf15719749ff9e20d481ccfc5f35147
RAGNOARAKNOS <ragnocorp@googlemail.com>
2016-07-29 16:42:13 +0100

    Added some crappy placeholder logic

---
+++ main.js
@@ -0,0 +1,25 @@
+var roleBuilder = require('./roles/roleBuilder');
+var roleHarvester = require('./roles/roleHarvester');
+
+module.exports.loop = function ()
+{
+
+  for(var name in Game.rooms)
+  {
+    console.log('Room "'+name+'" has '+Game.rooms[name].energyAvailable+' energy');
+  }
+
+  for(var name in Game.creeps)
+  {
+    var creep = Game.creeps[name];
+    
+    if(creep.memory.role == 'harvester')
+    {
+        roleHarvester.run(creep);
+    }
+    if(creep.memory.role == 'builder')
+    {
+        roleBuilder.run(creep);
+    }
+  }
+}
