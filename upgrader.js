12502e10598d07beeec4a12fab042de5ab26f1c6
RAGNOARAKNOS <ragnocorp@googlemail.com>
2016-07-29 18:25:18 +0100

    debug needed

--- upgrader.js
+++ upgrader.js
@@ -7,22 +7,26 @@ var roleUpgrader =
     {
       // switch state
       creep.memory.working = false;
+      console.log('P1');
     }
     // if creep is harvesting energy but is full
     else if (creep.memory.working == false && creep.carry.energy == creep.carryCapacity)
     {
       // switch state
       creep.memory.working = true;
+      console.log('P2');
     }
 
     // if creep is supposed to transfer energy to the controller
     if (creep.memory.working == true)
     {
+      console.log('P3');
       // try to upgrade the controller
       if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE)
       {
         // if not in range, move towards the controller
         creep.moveTo(creep.room.controller);
+        console.log('P4');
       }
       // if creep is supposed to harvest energy from source
       else
@@ -32,6 +36,7 @@ var roleUpgrader =
         // try to harvest energy, if the source is not in range
         if (creep.harvest(source) == ERR_NOT_IN_RANGE)
         {
+          console.log('P5');
           // move towards the source
           creep.moveTo(source);
         }
